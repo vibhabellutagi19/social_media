11829efaf2baddf5624e7a428d82b981837b1fe9
vibhabellutagi19 <vibhavari.bellutagi@gmail.com>
2024-08-08 22:23:38 +0530

    add cors

--- app/main.py
+++ app/main.py
@@ -1,17 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .config import Settings
 from app.routers import auth, vote
-from . import models
-from .database import engine
 from .routers import post, users
 
 
 app_settings = Settings()
+origins = [
+    "https://www.google.com/",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(users.router)
